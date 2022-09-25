e0e0ade5d5e13a9f72adcd7873fb07f30a450a87
lucasafonsobastos <lucasafonsob@hotmail.com>
2022-09-25 20:25:39 -0300

    Melhorando o servisor node

--- index.js
+++ index.js
@@ -3,7 +3,26 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    console.log("Servidor esta rodando")
+    //console.log("Servidor esta rodando")
+
+    res.writeHead(200, {"Content-Type": "text/plain"})
+    
+    switch(req.url){
+        case "/":
+            res.end("Voce esta na HOME PAGE")
+            break;
+        case "/contato":
+            res.end("Página de Contatos")
+            break;
+
+        default:
+            res.end("Voce esta no nosso servidor")
+    }
+    
+    //res.end("Meu primeio servidor")
+
+
+
 }).listen(3000);
 
 
