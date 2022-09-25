9a8edd2a72501ad1bd726fcf903720072dd7be31
lucasafonsobastos <lucasafonsob@hotmail.com>
2022-09-25 20:05:04 -0300

    testando servido com node

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log("Ola Projeto-X")
+//console.log("Ola Projeto-X")
+
+const http = require('http');
+
+http.createServer((req, res) => {
+    console.log("Servidor esta rodando")
+}).listen(3000);
+
+
 
 // A INTENSÃO É TESTAR AS ATUALIZAÇÕES E VERSIONAMENTO CRIADOS PELO GIT
