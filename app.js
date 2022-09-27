56c2b3cae3b9fb9e75f01144440cedaa3e5380e5
lucasafonsobastos <lucasafonsob@hotmail.com>
2022-09-26 21:45:08 -0300

    iniciando com express

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// OLA MUNDO!
+const express = require('express');
+
+let app = express();
+
+app.listen(3000, (req, res)=>{
+    console.log("Servidor rodando no endereço http://localhost:3000")
+});
