aad6bda184d33b6ec960e667f04c85dfab0ad33c
lucasafonsobastos <lucasafonsob@hotmail.com>
2022-09-26 21:58:19 -0300

    atualizando a função espress

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 let app = express();
 
-app.listen(3000, (req, res)=>{
+app.get('/', function (req, res) {
+    res.send("Agora esta funcionando a rota '/' ")
+});
+
+
+// sempre deve ficar no final do arquivo
+app.listen(3000, function(){
     console.log("Servidor rodando no endereço http://localhost:3000")
 });
