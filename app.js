88edecd70d1e833cb5cc43eb42b690a55f6a24c0
lucasafonsobastos <lucasafonsob@hotmail.com>
2022-09-27 21:00:03 -0300

    criando index.html

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require('express');
 let app = express();
 
 app.get('/', function (req, res) {
-    res.send("Agora esta funcionando a rota '/' ")
+    res.send("<h1>Título de nível 1</h1>")
 });
 
 
 // sempre deve ficar no final do arquivo
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Servidor rodando no endereço http://localhost:3000")
 });
